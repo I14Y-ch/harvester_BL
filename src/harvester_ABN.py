75d8868092724c47a66a028581b4d2047d792888
Lien Beroggi <163211986+lberoggi@users.noreply.github.com>
2025-04-08 10:46:45 +0200

    Update harvester_ABN.py

--- src/harvester_ABN.py
+++ src/harvester_ABN.py
@@ -33,7 +33,7 @@ def fetch_datasets_from_api() -> List[Dict]:
         graph = Graph()
         graph.parse(data=response.text, format='xml')
 
-        for dataset_uri in list(graph.subjects(RDF.type, DCAT.Dataset))[:3]:
+        for dataset_uri in list(graph.subjects(RDF.type, DCAT.Dataset))[:2]:
             print(f"Processing test dataset URI: {dataset_uri}")
             dataset = extract_dataset(graph, dataset_uri)
             
